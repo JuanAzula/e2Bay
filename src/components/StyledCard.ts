ea2ee6bd263393e998f155513112c7b6a7dd6627
JuanAzula <141027910+JuanAzula@users.noreply.github.com>
2024-02-26 16:50:39 +0100

    Finish styling products page

---
+++ src/components/StyledCard.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const StyledCard = styled.div`
+    width: 190px;
+    height: 254px;
+    margin-top: 9%;
+    margin-bottom: 6%;
+    margin-right: 5%;
+    background: white;
+    border-radius: 10px;
+    transition: border-radius 0.5s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+    box-shadow: inset 0 -3em 3em rgba(0,0,0,0.1),
+             0 0  0 2px rgb(190, 190, 190),
+             0.3em 0.3em 1em rgba(0,0,0,0.3);
+    `
--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -1,6 +1,7 @@
 // import './Products.css'
 import { SearchProvider } from '../../context/search.tsx'
 import { type ProductsType } from '../../interfaces/productsType.ts'
+import { StyledCard } from '../StyledCard.ts'
 import { StyledLink } from '../StyledLinks.ts'
 import { Filters } from './Filters.tsx'
 import SearchBar from './SearchBar.tsx'
@@ -14,21 +15,19 @@ export function Products ({ products }: { products: ProductsType['products'] | u
             </SearchProvider>
             <Filters />
 
-            <ul>
+            <ul className='card-container'>
                 {products?.map((product) => {
                   return (
-                        <li key={product.id}>
+                        <StyledCard key={product.id}>
                             <StyledLink to={`/products/${product.id}`}>
                                 <img className="products__img" src={product.image}
                                     alt={product.name}
                                 />
-                            </StyledLink>
                                 <div>
-                                <StyledLink to={`/products/${product.id}`}>
                                     <strong>{product.name}</strong> <span>${product.price}</span>
-                                </StyledLink>
                                 </div>
-                        </li>
+                                </StyledLink>
+                        </StyledCard>
                   )
                 })}
             </ul>
